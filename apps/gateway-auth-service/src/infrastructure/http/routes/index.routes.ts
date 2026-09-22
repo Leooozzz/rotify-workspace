a7346f5061a7c79ede220a6e3515a5e41468896c
leooozzz <leozeraffcanal2405@gmail.com>
2026-09-22 18:30:52 -0300

    feat:schema-prisma orders

--- apps/gateway-auth-service/src/infrastructure/http/routes/index.routes.ts
+++ apps/gateway-auth-service/src/infrastructure/http/routes/index.routes.ts
@@ -1,8 +1,33 @@
-import { Router } from "express";
+import { Router, type Request } from "express";
 import { usersRoutes } from "./users.routes";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { env } from "../../../config/env.config";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
+
+router.use(
+  "/orders",
+  ensureAuthenticated,
+  createProxyMiddleware({
+    target: env.ORDERS_SERVICE,
+    changeOrigin: true,
+    on: {
+      proxyReq: (proxyReq, req) => {
+        const auth = (req as Request).auth;
+
+        if (!auth) return;
+
+        proxyReq.setHeader("x-user-id", auth.userId);
+
+        if (auth.companyId) {
+          proxyReq.setHeader("x-company-id", auth.companyId);
+        }
+      },
+    },
+  }),
+);
 
 export { router };
